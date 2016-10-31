1846a5e30ee91a91c0526d2a5c8e988cd176cfb6
Nathalie <Nathalie@192.168.0.18>
2016-11-01 00:12:08 +0100

    classes

---
+++ src/test/java/usrTest/UserTest.java
@@ -0,0 +1,18 @@
+/*Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.*/
+
+package usrTest;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
