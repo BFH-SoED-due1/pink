0b428c3b9d11640f04a8219815bcf1d9d2b736df
Nathalie <Nathalie@ToshibaI7.bfh.ch>
2016-10-31 10:44:46 +0100

    sds

--- src/main/java/srs/A.java
+++
@@ -1,8 +0,0 @@
-/*Copyright (c) 2016 Berner Fachhochschule, Switzerland.
- * Project Smart Reservation System.
- * Distributable under GPL license. See terms of license at gnu.org.*/
-package srs;
-
-public class A {
-
-}
