f1e682dbc52e7a9d28a0ae4acfad1f2f51011487
Nathalie <Nathalie@ToshibaI7.bfh.ch>
2016-10-31 10:39:24 +0100

    s2

---
+++ src/main/java/srs/A.java
@@ -0,0 +1,9 @@
+/*Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.*/
+
+package srs;
+
+public class A {
+
+}
--- src/test/java/srsTest/RoomTest.java
+++ src/test/java/srsTest/RoomTest.java
@@ -1,3 +1,7 @@
+/*Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.*/
+
 package srsTest;
 
 import static org.junit.Assert.*;
