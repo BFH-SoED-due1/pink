858cb32599bdfb8165370b44bec711dbf1ff24ec
Nathalie <Nathalie@ToshibaI7.bfh.ch>
2016-10-31 10:24:34 +0100

    new

---
+++ src/main/java/srs/Room.java
@@ -0,0 +1,43 @@
+package srs;
+
+public class Room {
+
+	//Room
+	private int size;
+	private String name;
+	private String description;
+	
+	
+	public Room(int s, String n, String d) {
+		this.size = s;
+		this.name = n;
+		this.description = d;
+	}
+
+	//Setter
+	public void setSize(int s) {
+		this.size = s;
+	}
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+
+	public void setDescription(String d) {
+		this.description = d;
+	}
+	
+	//Getter
+	public int getSize() {
+		return this.size;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+}
+
---
+++ src/test/java/srsTest/BookerTest.java
@@ -0,0 +1,130 @@
+package srsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import srs.Booker;
+import srs.BookerAddress;
+import srs.BookerName;
+
+public class BookerTest {
+	//Test booker, bookeraddress and bookername class
+	private final String firstName = "Hans";
+	private String lastName = "Muster";
+	private String country = "Switzerland";
+	private String street = "Bernstrasse";
+	private String streetNumber = "27A";
+	private int postcode = 3000;
+	private String village = "Bern";
+	private String email = "d@h.ch";
+	
+	private String newFirstName = "Fritz";
+	private String newLastName = "Meier";
+	private String newCountry = "Switzerland";
+	private String newStreet = "Seestrasse";
+	private String newStreetNumber = "45";
+	private int newPostcode = 1200;
+	private String newVillage = "Genf";
+	private String newEmail = "d@ha.ch";
+
+
+	//Name	@Test
+	public void testName() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+		assertEquals(booker.getName(), firstName + " " + lastName);
+		booker.setName(newFirstName, newLastName);
+		assertEquals(newFirstName + " " + newLastName, booker.getName());
+	}
+
+	@Test
+	public void testFirstName() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+		
+		assertEquals(firstName, booker.getFirstName());
+		booker.setFirstName(newFirstName);
+		assertEquals(newFirstName, booker.getFirstName());
+	}
+	
+	public void testLastName() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		assertEquals(lastName, booker.getLastName());
+		booker.setLastName(newLastName);
+		assertEquals(newLastName, booker.getLastName());
+	}
+	
+	//Address
+	@Test
+	public void testAddress() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		assertEquals(country + " " + street + " " + streetNumber + " " + postcode + " " + village + " " + email, booker.getAddress());
+		booker.setAddress(newCountry, newStreet, newStreetNumber, newPostcode, newVillage, newEmail);
+		assertEquals(newCountry + " " + newStreet + " " + newStreetNumber + " " + newPostcode + " " + newVillage + " " + newEmail, booker.getAddress());
+	}
+	
+	@Test
+	public void testCountry() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		assertEquals(country, booker.getCountry());
+		booker.setCountry(newCountry);
+		assertEquals(newCountry, booker.getCountry());
+	}
+	
+	@Test
+	public void testStreet() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		assertEquals(street, booker.getStreet());
+		booker.setStreet(newStreet);
+		assertEquals(newStreet, booker.getStreet());
+	}
+	
+	@Test
+	public void testStreetNumber() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		assertEquals(streetNumber, booker.getStreetNumber());
+		booker.setStreetNumber(newStreetNumber);
+		assertEquals(newStreetNumber, booker.getStreetNumber());
+	}
+	
+	@Test
+	public void testPostcode() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		assertEquals(postcode, booker.getPostcode());
+		booker.setPostcode(newPostcode);
+		assertEquals(newPostcode, booker.getPostcode());
+	}
+	
+	@Test
+	public void testVillage() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+		
+		assertEquals(village, booker.getVillage());
+		booker.setVillage(newVillage);
+		assertEquals(newVillage, booker.getVillage());
+	}
+	
+	@Test
+	public void testEmail() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		assertEquals(email, booker.getEmail());
+		booker.setEmail(newEmail);
+		assertEquals(newEmail, booker.getEmail());
+	}
+	
+	@SuppressWarnings("null")
+	@Test(expected = NullPointerException.class)
+	public void testDestrObj() {
+		Booker booker = new Booker(new BookerName(firstName, lastName), new BookerAddress(country, street, streetNumber, postcode, village, email));
+
+		booker = null;
+
+		assertEquals(firstName + " " + lastName, booker.getName());
+	}	
+}
