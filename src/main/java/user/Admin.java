0fb615970aeb161265f0121296209f15509605d7
Nathalie <Nathalie@ToshibaI7.bfh.ch>
2016-11-07 12:00:39 +0100

    header

--- src/main/java/user/Admin.java
+++ src/main/java/user/Admin.java
@@ -1,7 +1,6 @@
 /*Copyright (c) 2016 Berner Fachhochschule, Switzerland.
  * Project Smart Reservation System.
  * Distributable under GPL license. See terms of license at gnu.org.*/
-
 package user;
 
 public class Admin implements I_SystemUser {
