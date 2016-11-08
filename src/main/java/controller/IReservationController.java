163332b30336fc2ea5a5917126051eedc5a0be00
Nathalie <Nathalie@ToshibaI7.bfh.ch>
2016-11-08 16:24:15 +0100

    controller

---
+++ src/main/java/controller/IReservationController.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package controller;
+
+public interface IReservationController {
+
+}
