d4a5ca9a74233de58bb8ee70f2dd538c62afa5eb
Nathalie <Nathalie@192.168.0.18>
2016-11-08 21:49:08 +0100

    tests

---
+++ src/test/java/controllerTest/BookerControllerTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package controllerTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookerControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
