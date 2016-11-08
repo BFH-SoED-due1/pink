0eed38b11e632072f5557082e799a579a33a5268
Nathalie <Nathalie@ToshibaI7.bfh.ch>
2016-11-08 16:34:01 +0100

    controller

---
+++ src/main/java/controller/ILogInController.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package controller;
+
+public interface ILogInController {
+	// Responsible for login / logout
+
+}
---
+++ src/main/java/controller/implementation/LogInController.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package controller.implementation;
+
+import controller.ILogInController;
+
+public class LogInController implements ILogInController {
+	// Implementation for login / logout
+}
