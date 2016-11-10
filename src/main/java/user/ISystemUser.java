e548e41acab9f49cb10e75bc1837032636b0a9dd
Nathalie <Nathalie@ToshibaI7.bfh.ch>
2016-11-10 11:38:43 +0100

    javadoc

--- src/main/java/user/ISystemUser.java
+++ src/main/java/user/ISystemUser.java
@@ -7,17 +7,17 @@ package user;
 
 public interface ISystemUser {
 
-	/*Setters*/
+	/**Setters*/
 	/*set a name for a user/admin for srs*/
 	public void setName(String name);
 
-	/*set a first name for a user/admin for srs*/
+	/**set a first name for a user/admin for srs*/
 	public void setFristName(String fName);
 
-	/*set a login for a user/admin for srs*/
+	/**set a login for a user/admin for srs*/
 	public void setLoggin(String loggin);
 
-	/*set a password for a user/admin for srs*/
+	/**set a password for a user/admin for srs*/
 	public void setPassword(String pw);
 
 	
@@ -26,43 +26,43 @@ public interface ISystemUser {
 	/*set right to user*/
 	public void setUser();
 
-	/*set right to admin (privileged mode)*/
+	/**set right to admin (privileged mode)*/
 	public void setAdmin();
 
-	/*remove a user*/
+	/**remove a user*/
 	public void removeUser();
 
-	/*remove an admin*/
+	/**remove an admin*/
 	public void removeAdmin();
 
 	/*getter*/
-	/*get the name
+	/**get the name
 	 * @return the name of a user/admin
 	 * */
 	public String getName();
 
-	/*get the first name
+	/**get the first name
 	 * @return the first name of a user/admin
 	 * */
 	public String getFirstName();
 
 	
-	/*get the login
+	/**get the login
 	 * @return users/admins login
 	 * */
 	public String getLogin();
 
-	/*get the password
+	/**get the password
 	 * @return the users/admins password*/
 	public String getPassword();
 
 	/*rights*/
-	/*check the rights
+	/**check the rights
 	 * @return true if user has privileged right, admin right
 	 * */
 	public boolean isAdmin();
 
-	/*check the right
+	/**check the right
 	 * @return true if user has non-privileged right, user rights*/
 	public boolean isUser();
 }
