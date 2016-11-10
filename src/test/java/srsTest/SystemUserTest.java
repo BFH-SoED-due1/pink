f306598e871c78cff58aad9099a0ee558cdf2cc0
Damian Hänzi <dha@itok.ch>
2016-11-10 20:53:28 +0100

    a

--- src/test/java/srsTest/SystemUserTest.java
+++ src/test/java/srsTest/SystemUserTest.java
@@ -3,7 +3,7 @@
  * Project Smart Reservation System.
  * Distributable under GPL license. See terms of license at gnu.org.
  */
-package controllerTest;
+package srsTest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
