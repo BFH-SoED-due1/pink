9ed21983a49084c806badaeccaf6ca61585db2fe
bandn2 <bandn2@users.noreply.github.com>
2016-11-11 14:19:44 +0100

    Add files via upload

---
+++ src/main/java/controller/ILogInController.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package controller;
+
+public interface ILogInController {
+
+	/**
+	 * Sets the status to logged in
+	 * @param login
+	 *            the login of a specific user
+	 * @param password
+	 *            the password of a specific user
+	 */
+	public void userLogin(String login, String password);
+
+	/**
+	 * Sets the status to logged out
+	 * @param login
+	 *            the login of a specific user
+	 */
+	public void userLogout(String login);
+}
