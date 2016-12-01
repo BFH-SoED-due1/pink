e4b98537e81429620d56bffcdc817c8990c1dce8
Nathalie <bandn2@bfh.ch>
2016-12-01 18:14:02 +0100

    checkstyle

--- src/main/java/model/JPADataAccess.java
+++ src/main/java/model/JPADataAccess.java
@@ -1,4 +1,8 @@
-/**Copyright(c)2016 Berner Fachhochschule,Switzerland.*Project Smart Reservation System.*Distributable under GPL license.See terms of license at gnu.org.*/
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
 package model;
 
 import control.DataAccess;
