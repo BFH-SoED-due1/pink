4a81799f26ee31cee6e135e082a096d191e715cc
Nathalie <bandn2@bfh.ch>
2016-12-05 15:14:13 +0100

    ....

--- src/main/java/view/MyUI.java
+++ src/main/java/view/MyUI.java
@@ -1,7 +1,11 @@
-/**Copyright(c)2016 Berner Fachhochschule,Switzerland.
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
  * Project Smart Reservation System.
- * Distributable under GPL license.See terms of license at gnu.org.*/
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
 package view;
+
+//test
 
 import javax.servlet.annotation.WebServlet;
 
@@ -10,19 +14,19 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-/***
- * This UI is the application entry point.A UI may either represent a
- * browser*window(or tab)or some part of a html page where a Vaadin application
- * is*embedded.*
+/**
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
  * <p>
- * *The UI is initialized using{
- * 
- * @link #init(VaadinRequest)}.This method is*intended to be overridden to add
- *       component to the user interface and* initialize non- component
- *       functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
 
 @SuppressWarnings("serial")
@@ -32,12 +36,20 @@ public class MyUI extends UI {
 	@Override
 	protected void init(VaadinRequest vaadinRequest) {
 		final VerticalLayout layout = new VerticalLayout();
-		Button button = new Button("Login in SRS");
+
+		final TextField name = new TextField();
+		name.setCaption("Type your name here:");
+
+		Button button = new Button("Click Me");
 		button.addClickListener(e -> {
-			new SysUserLoginView();
+			layout.addComponent(new Label("Thank you very much, " + name.getValue() + ", it works!"));
 		});
 
-		layout.addComponent(button);
+		layout.addComponents(name, button);
+		layout.setMargin(true);
+		layout.setSpacing(true);
+		// testtest
+		setContent(layout);
 	}
 
 	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
