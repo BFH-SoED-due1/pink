214a3cf000dbecea63bc5ede80c5db89ce38eeaf
Nathalie <bandn2@bfh.ch>
2016-12-05 23:13:45 +0100

    test gui

--- src/main/java/view/StartScreenView.java
+++ src/main/java/view/StartScreenView.java
@@ -5,8 +5,6 @@
  */
 package view;
 
-//test
-
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
@@ -15,7 +13,6 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -31,29 +28,31 @@ import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Theme("mytheme")
-public class MyUI extends UI {
+public class StartScreenView extends UI {
 
 	@Override
 	protected void init(VaadinRequest vaadinRequest) {
 		final VerticalLayout layout = new VerticalLayout();
 
-		final TextField name = new TextField();
-		name.setCaption("Type your name here:");
+		final Label title = new Label();
+		title.setCaption("SRS - Smart Reservation System");
 
-		Button button = new Button("Click Me");
-		button.addClickListener(e -> {
-			layout.addComponent(new Label("Thank you very much, " + name.getValue() + ", it works!"));
+		Button btn = new Button("Login");
+		btn.addClickListener(e -> {
+			// layout.addComponent(new Label(title.getValue()));
+			/** TODO */
+
 		});
 
-		layout.addComponents(name, button);
+		layout.addComponents(title, btn);
 		layout.setMargin(true);
 		layout.setSpacing(true);
-		// testtest
 		setContent(layout);
+
 	}
 
 	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	@VaadinServletConfiguration(ui = StartScreenView.class, productionMode = false)
 	public static class MyUIServlet extends VaadinServlet {
 	}
 }
