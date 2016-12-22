9a88747249ba6bb89be417f80757c39853f8f63f
Damian Hänzi <dha@itok.ch>
2016-12-22 15:01:17 +0100

    new

---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/view/views/helpers/Menu.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
+package ch.bfh.ti.soed.hs16.srs.view.views.helpers;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Layout;
+
+/**
+ * Created by tambur on 04.12.2016.
+ */
+public class Menu extends CustomComponent implements View {
+
+	private Layout menu;
+	private Button myReservationButton;
+	private Button roomsButton;
+	private Navigator navigator;
+	private String CLASSNAME = "menu";
+
+	public Menu(Navigator nav) {
+
+		/*---------------------------------
+		initalize Objects
+		---------------------------------*/
+		this.navigator = nav;
+		this.menu = new HorizontalLayout();
+		this.myReservationButton = new Button("my reservation");
+		this.roomsButton = new Button("rooms");
+
+		/*-------------------------------
+		add to css
+		-------------------------------*/
+		menu.setPrimaryStyleName(CLASSNAME);
+		myReservationButton.setPrimaryStyleName(CLASSNAME + "-button");
+		roomsButton.setPrimaryStyleName(CLASSNAME + "-button");
+
+		/*-------------------------------
+		add Buttons to Layout
+		--------------------------------*/
+		menu.addComponent(myReservationButton);
+		menu.addComponent(roomsButton);
+
+		/*------------------------------
+		Event Handling Buttons
+		------------------------------*/
+		myReservationButton.addClickListener((Button.ClickListener) event -> navigator.navigateTo("my Reservation"));
+		roomsButton.addClickListener((Button.ClickListener) event -> navigator.navigateTo("rooms"));
+
+	}
+
+	public Layout getMenu() {
+		return menu;
+	}
+
+	@Override
+	public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+
+	}
+}
